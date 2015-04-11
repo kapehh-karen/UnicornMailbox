06f97dba8d14b89ad2dc5cd20818985ccf75268a
kapehh <git@kapehh.net>
2015-04-11 23:15:39 +0400

    add serializer

--- src/me/kapehh/UnicornMailbox/Main.java
+++ src/me/kapehh/UnicornMailbox/Main.java
@@ -6,15 +6,20 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
 import java.beans.XMLEncoder;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
  * Created by Karen on 08.04.2015.
  */
 public class Main extends JavaPlugin {
+    byte[] tmp = null;
+
     @Override
     public void onDisable() {
         super.onDisable();
@@ -26,13 +31,27 @@ public class Main extends JavaPlugin {
             @Override
             public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
                 Player player = (Player) sender;
-                ItemStack itemStack = player.getItemInHand();
+                ItemStack itemStack;
                 try {
-                    BukkitObjectOutputStream bukkitObjectOutputStream = new BukkitObjectOutputStream(System.out);
-                    bukkitObjectOutputStream.writeObject(itemStack);
-                    bukkitObjectOutputStream.flush();
-                    //bukkitObjectOutputStream.close();
+                    if (tmp == null) {
+                        itemStack = player.getItemInHand();
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        BukkitObjectOutputStream bukkitObjectOutputStream = new BukkitObjectOutputStream(byteArrayOutputStream);
+                        bukkitObjectOutputStream.writeObject(itemStack);
+                        bukkitObjectOutputStream.flush();
+                        bukkitObjectOutputStream.close();
+                        tmp = byteArrayOutputStream.toByteArray();
+                        player.sendMessage("Geted: " + itemStack);
+                    } else {
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(tmp);
+                        BukkitObjectInputStream bukkitObjectInputStream = new BukkitObjectInputStream(byteArrayInputStream);
+                        itemStack = (ItemStack) bukkitObjectInputStream.readObject();
+                        player.setItemInHand(itemStack);
+                        player.sendMessage("Seted: " + itemStack);
+                    }
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
                 return true;
--- src/me/kapehh/UnicornMailbox/serialize/ItemStackSerializer.java
+++ src/me/kapehh/UnicornMailbox/serialize/ItemStackSerializer.java
@@ -1,12 +1,33 @@
 package me.kapehh.UnicornMailbox.serialize;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Karen on 08.04.2015.
  */
 public class ItemStackSerializer {
-    public static String toString(ItemStack itemStack) {
-        return "";
+
+    private ItemStackSerializer() { /* blocked */ }
+
+    public static byte[] toBytes(ItemStack itemStack) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        BukkitObjectOutputStream bukkitObjectOutputStream = new BukkitObjectOutputStream(byteArrayOutputStream);
+        bukkitObjectOutputStream.writeObject(itemStack);
+        bukkitObjectOutputStream.flush();
+        bukkitObjectOutputStream.close();
+        return byteArrayOutputStream.toByteArray();
     }
+
+    public static ItemStack fromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        BukkitObjectInputStream bukkitObjectInputStream = new BukkitObjectInputStream(byteArrayInputStream);
+        return (ItemStack) bukkitObjectInputStream.readObject();
+    }
+
 }
